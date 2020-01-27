d47bb777295ed4ee944479209fdff4deb6845c71
Usuario <Usuario@DESKTOP-2QMCK54>
2020-01-27 23:00:26 +0100

    pruebas

--- src/transferObjects/ApunteBean.java
+++ src/transferObjects/ApunteBean.java
@@ -19,7 +19,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement(name="apunte")
 public class ApunteBean implements Serializable {
-    //private static final long serialVersionUID=1L;
+    private static final long serialVersionUID=1L;
     
     private final SimpleIntegerProperty idApunte;
     private final SimpleStringProperty titulo;
