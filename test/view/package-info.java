049fe45540cf600e2cfd8a72f7ab421dbe336e7a
2dam <2dam@HZ341352.TartangaLH.eus>
2020-01-28 08:27:01 +0100

    Test mejorado 2

---
+++ test/view/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * El paquete que engloba todas las clases de test de la aplicación.
+ */
+package view;
