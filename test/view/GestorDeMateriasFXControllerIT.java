a2bab77d9ec09f8b9f8d4362fc906a211f456772
2dam <2dam@MACINF01.TartangaLH.eus>
2020-01-29 13:01:17 +0100

    29/01/2020 test

--- test/view/GestorDeMateriasFXControllerIT.java
+++ test/view/GestorDeMateriasFXControllerIT.java
@@ -1,24 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
+import javafx.scene.control.TableView;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import org.testfx.framework.junit.ApplicationTest;
 
 /**
- *
+ * Clase 
  * @author Luis
  */
-public class GestorDeMateriasFXControllerIT {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class GestorDeMateriasFXControllerIT extends ApplicationTest{
     
+    private final String tituloApunte="El apunte test";
+    private final String descApunte="La descripción del apunte test";
+    private final String tituloEditado="Titulo del apunte editado";
+    private final String precio="12";
+    private final int MIN_CARACTERES=3;
+    private final int MAX_CARACTERES=250;
+    private String fraseErrorTitulo="Titulo (Min "+MIN_CARACTERES+" car. | Max "+MAX_CARACTERES+" car.)";
+    private String fraseErrorDesc="Descripción (Min "+MIN_CARACTERES+" car. | Max "+MAX_CARACTERES+" car.)";
+    private  final String MAX_TEXT="ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD"+
+            "ASDASDASDASDASDASDASDASDASDASDASDASD";
+    private TableView table;
     public GestorDeMateriasFXControllerIT() {
     }
-
-    @Test
-    public void testSomeMethod() {
+    @Override
+    public void start(Stage stage) throws Exception{
+        new ClienteA4().start(stage);
     }
     
 }
